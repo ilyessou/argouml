98cab7d9d97d92e6a52eb93e425563f2874d5c41
nobody <nobody@a161b567-7d1e-0410-9ef9-912c70fedb3f>
2002-10-09 21:00:56 +0000

    This commit was manufactured by cvs2svn to create branch 'work_i18n_jhraigniac'.

---
+++ branches/work_i18n_jhraigniac/src_new/org/argouml/uml/ui/behavior/collaborations/PropPanelInteraction.java
@@ -0,0 +1,106 @@
+// Copyright (c) 1996-2002 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies.  This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+package org.argouml.uml.ui.behavior.collaborations;
+
+import java.awt.Color;
+
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+
+import org.argouml.application.api.Argo;
+import org.argouml.swingext.LabelledLayout;
+import org.argouml.uml.ui.PropPanelButton;
+import org.argouml.uml.ui.UMLLinkedList;
+import org.argouml.uml.ui.UMLList;
+import org.argouml.uml.ui.foundation.core.PropPanelModelElement;
+import org.argouml.util.ConfigLoader;
+
+import ru.novosoft.uml.behavior.collaborations.MInteraction;
+
+/**
+ * Proppanel for interactions. 
+ * @author jaap.branderhorst@xs4all.nl
+ */
+public class PropPanelInteraction extends PropPanelModelElement {
+
+    public PropPanelInteraction() {
+        super("Interaction", _interactionIcon, ConfigLoader.getTabPropsOrientation());
+ //       removeNavigationListener(this);
+ //       addNavigationListener(this);
+	
+    	addField(Argo.localize("UMLMenu", "label.name"), nameField);  	
+    	addField(Argo.localize("UMLMenu", "label.stereotype"), stereotypeBox);
+        JList contextList = new UMLLinkedList(this, new UMLInteractionCollaborationListModel(this));
+        contextList.setVisibleRowCount(1);
+        JScrollPane contextScroll = new JScrollPane(contextList);
+        addField(Argo.localize("UMLMenu", "label.context"), contextScroll);
+        
+        add(LabelledLayout.getSeperator());
+        /*    
+        JList messagesList = new UMLList(new UMLInteractionMessagesListModel(this,"messages",true),true);
+      	messagesList.setBackground(getBackground());
+      	messagesList.setForeground(Color.blue);
+        */
+        JList messagesList = new UMLLinkedList(this, new UMLInteractionMessagesListModel(this));
+      	JScrollPane messagesScroll= new JScrollPane(messagesList); 	
+        addField(Argo.localize("UMLMenu", "label.messages"), messagesScroll);
+    	
+    	new PropPanelButton(this,buttonPanel,_navUpIcon, Argo.localize("UMLMenu", "button.go-up"),"navigateUp",null);
+        new PropPanelButton(this,buttonPanel,_navBackIcon, Argo.localize("UMLMenu", "button.go-back"),"navigateBackAction","isNavigateBackEnabled");
+        new PropPanelButton(this,buttonPanel,_navForwardIcon, Argo.localize("UMLMenu", "button.go-forward"),"navigateForwardAction","isNavigateForwardEnabled");
+        new PropPanelButton(this,buttonPanel,_deleteIcon, Argo.localize("UMLMenu", "button.delete-attribute"),"removeElement",null);
+    	
+    	/*
+    	addCaption("Messages:",0,1,0);
+    	JList messageList = new UMLList(new UMLAssociationRoleMessageListModel(this,"message",true), true);
+    	messageList.setBackground(getBackground());
+    	messageList.setForeground(Color.blue);
+    	addField(new JScrollPane(messageList),0,1,1);
+    	*/
+	
+
+	}
+
+	
+
+	/**
+	 * Used to determine which stereotypes are legal with an interaction. At 
+     * the moment, only the stereotypes of namespace and generlizable elements
+     * are shown.
+	 * @see org.argouml.uml.ui.PropPanel#isAcceptibleBaseMetaClass(String)
+	 */
+	protected boolean isAcceptibleBaseMetaClass(String baseClass) {
+		return (baseClass.equals("Interaction") || baseClass.equals("Modelelement"));
+	}
+	
+	/**
+	 * Navigates to the owning collaboration
+	 * @see org.argouml.uml.ui.foundation.core.PropPanelModelElement#navigateUp()
+	 */
+	public void navigateUp() {
+		navigateTo(((MInteraction)getTarget()).getContext());
+	}
+
+}
+
