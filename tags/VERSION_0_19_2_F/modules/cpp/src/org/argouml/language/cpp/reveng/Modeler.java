73cf7feeecbc26e7d6db46a78fc4aafaa1626e9a
nobody <nobody@a161b567-7d1e-0410-9ef9-912c70fedb3f>
2005-06-09 16:00:50 +0000

    This commit was manufactured by cvs2svn to create tag 'VERSION_0_19_2_F'.

---
+++ tags/VERSION_0_19_2_F/modules/cpp/src/org/argouml/language/cpp/reveng/Modeler.java
@@ -0,0 +1,161 @@
+// $Id$
+// Copyright (c) 1996-2005 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies.  This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+/*REMOVE_BEGIN*/
+package org.argouml.language.cpp.reveng;
+
+import java.util.List;
+
+/**
+ * The Modeler receives calls from some reverse engineering system and is able
+ * to transform this information into model elements. It expects to receive
+ * calls in a logic sequence:
+ * <ol>
+ * <li>beginClassDeclaration</li>
+ * <li>derivedFromClass</li>
+ * <li>beginMemberDeclaration</li>
+ * <li>endMemberDeclaration</li>
+ * <li>endClassDeclaration</li>
+ * </ol>
+ * 
+ * @author Luis Sergio Oliveira
+ * @version 0.00
+ * @since 0.19.1
+ */
+public interface Modeler {
+
+    /**
+     * Signals the begin of the translation unit.
+     */
+    void beginTranslationUnit();
+
+    /**
+     * Signals the end of the translation unit.
+     */
+    void endTranslationUnit();
+
+    /**
+     * Signals that we entered the given namespace scope.
+     * 
+     * @param ns
+     *            The namespace identifier. For unnamed namespaces it is an
+     *            empty <code>String</code>.
+     */
+    void enterNamespaceScope(String ns);
+
+    /**
+     * Signals the exit of the namespace scope.
+     */
+    void exitNamespaceScope();
+
+    /**
+     * Signal a new alias for the given namespace.
+     * 
+     * @param ns Identifier of the namespace for which an alias will be created.
+     * @param alias Identifier of the new namespace alias.
+     */
+    void makeNamespaceAlias(String ns, String alias);
+
+    /**
+     * Signal the begin of a new class definition
+     * 
+     * @param oType
+     *            The type of the specifier from the CPPvariables class, it is
+     *            one of the "OT_*" values, e.g., OT_CLASS.
+     * @param identifier The class identifier (name).
+     */
+    void beginClassDefinition(String oType, String identifier);
+
+    /**
+     * Signals the end of a class definition.
+     */
+    void endClassDefinition();
+
+    /**
+     * Signals that the given access specifier was found.
+     * 
+     * @param accessSpec The access specifier, e.g., "public".
+     */
+    void accessSpecifier(String accessSpec);
+
+    /**
+     * Signals the begin of a function declaration.
+     */
+    void beginFunctionDeclaration();
+
+    /**
+     * Signals the end of a function declaration.
+     */
+    void endFunctionDeclaration();
+
+    /**
+     * Reports the declaration specifiers; "inline", "virtual", "friend", etc.
+     * 
+     * @param declSpecs
+     *            The declaration specifiers for the current declaration.
+     */
+    void declarationSpecifiers(List declSpecs);
+
+    /**
+     * Reports the type simple type specifiers (buit-in, like int, char,
+     * unsigned char, etc).
+     * 
+     * @param sts The simple type specifiers,
+     */
+    void simpleTypeSpecifier(List sts);
+
+    /**
+     * Reports the declarator, of the entity being declared.
+     * @param id The identifier of the construct being declared.
+     */
+    void directDeclarator(String id);
+
+    /**
+     * Reports the storage class specifier.
+     * @param storageClassSpec The storage class specifier.
+     */
+    void storageClassSpecifier(String storageClassSpec);
+
+    /**
+     * Reports the type qualifier.
+     * @param typeQualifier "const" or "volatile".
+     */
+    void typeQualifier(String typeQualifier);
+
+    /**
+     * Signals the begin of a function definition.
+     */
+    void beginFunctionDefinition();
+
+    /**
+     * Signals the end of a function definition.
+     */
+    void endFunctionDefinition();
+
+    /**
+     * Reports the identifier of the function being declared or defined.
+     * @param identifier the function identifier.
+     */
+    void functionDirectDeclarator(String identifier);
+}
---
+++ tags/VERSION_0_19_2_F/src_new/README.txt
@@ -0,0 +1,57 @@
+Welcome to ArgoUML!
+
+(This file was last updated by $Author$ on $Date$.)
+
+In case you just downloaded and unzipped your ArgoUML distribution,
+here's how to proceed.
+
+First, you need the runtime environment for J2SE installed, get it
+from http://java.sun.com for your platform.
+
+At least a JRE 1.4 is required. Please let us know if you experience
+any JDK related problems. Try typing "java" in a console window to see
+if Java is successfully installed.  You should get a list of your
+options. If not, try reinstalling Java.
+
+Now (after unpacking the distro, which you obviously have done,
+because this file was in it) you should have a bunch of .jar files
+(argouml.jar, nsuml.jar and others...) in your directory.  Please do
+NOT unpack the .jars! Just leave them as jar files.
+
+Now you can just start the argouml.jar, on a Windows platform by
+double-clicking it. You can just as well type the following at a
+command line console: java -jar argouml.jar
+
+The same effect can be obtained by creating a batch-file (in the same
+dir as argouml.jar), with the following contents:
+  java -jar argouml.jar
+which can then be started via a shortcut on the desktop.
+
+Detailed instructions in running ArgoUML from a command line console
+on Windows:
+
+Suppose that you have downloaded ArgoUML to the C: drive, so your
+directory looks like this:
+  c:\ArgoUML
+Open your console (for M$ users this is the DOS prompt) and type
+  cd \
+to set the current directory to the root. Then type:
+  c\:>cd ArgoUML
+which will give you the prompt:
+  c:\ArgoUML>
+Then type: 
+  dir 
+to see everything in the ArgoUML folder. You should have the
+argouml.jar file present in this folder. Note that you need all
+downloaded jar files together in this directory (except if you
+downloaded modules. There jar files should be an the ext
+subdirectory).
+Then type
+  java -jar argouml.jar
+That's it, ArgoUML should start up now. 
+
+If not, there is a problem with the downloading on your computer.
+Please have a look at the FAQ and then consider posting an issue at
+http://argouml.tigris.org.
+
+Have fun!
