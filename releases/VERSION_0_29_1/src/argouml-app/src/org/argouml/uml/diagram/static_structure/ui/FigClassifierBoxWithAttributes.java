8519e39c93a6fe2f75cdd05c5df6eca3e47d8fa3
linus <linus@a161b567-7d1e-0410-9ef9-912c70fedb3f>
2009-10-08 18:04:09 +0000

    Creating the release 0.29.1

---
+++ releases/VERSION_0_29_1/src/argouml-app/src/org/argouml/uml/diagram/static_structure/ui/FigClassifierBoxWithAttributes.java
@@ -0,0 +1,240 @@
+// $Id$
+// Copyright (c) 2008-2009 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies. This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason. IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+package org.argouml.uml.diagram.static_structure.ui;
+
+import java.awt.Rectangle;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.swing.Action;
+
+import org.argouml.model.AddAssociationEvent;
+import org.argouml.model.AssociationChangeEvent;
+import org.argouml.model.AttributeChangeEvent;
+import org.argouml.model.Model;
+import org.argouml.model.RemoveAssociationEvent;
+import org.argouml.model.UmlChangeEvent;
+import org.argouml.ui.ArgoJMenu;
+import org.argouml.uml.diagram.AttributesCompartmentContainer;
+import org.argouml.uml.diagram.DiagramSettings;
+import org.argouml.uml.diagram.ui.FigAttributesCompartment;
+import org.argouml.uml.ui.foundation.core.ActionAddAttribute;
+
+/**
+ * A Fig for a ClassifierBox that adds an attributes compartment.
+ *
+ * @author Michiel
+ */
+public abstract class FigClassifierBoxWithAttributes extends FigClassifierBox 
+    implements AttributesCompartmentContainer {
+    
+    private FigAttributesCompartment attributesFigCompartment;
+    
+    /**
+     * Construct a Fig with owner, bounds, and settings.
+     * 
+     * @param owner the model element that owns this fig
+     * @param bounds the rectangle defining the bounds
+     * @param settings the rendering settings
+     */
+    public FigClassifierBoxWithAttributes(Object owner, Rectangle bounds,
+            DiagramSettings settings) {
+        super(owner, bounds, settings);
+        getAttributesCompartment(); // this creates the compartment fig
+    }
+
+    /**
+     * @return The graphics for the UML attributes (if any).
+     * @deprecated use getAttributesCompartment
+     */
+    protected FigAttributesCompartment getAttributesFig() {
+        return getAttributesCompartment();
+    }
+    
+    public Rectangle getAttributesBounds() {
+        return getAttributesCompartment().getBounds();
+    }
+
+    public boolean isAttributesVisible() {
+        return attributesFigCompartment != null 
+            && attributesFigCompartment.isVisible();
+    }
+    
+    /*
+     * Insert an Add Attribute entry before the default menu.
+     * 
+     * @see org.argouml.uml.diagram.static_structure.ui.FigClassifierBox#buildAddMenu()
+     */
+    @Override
+    protected ArgoJMenu buildAddMenu() {
+        ArgoJMenu addMenu = super.buildAddMenu();
+        Action addAttribute = new ActionAddAttribute();
+        addAttribute.setEnabled(isSingleTarget());
+        addMenu.insert(addAttribute, 0);
+        return addMenu;
+    }
+    
+    /**
+     * USED BY PGML.tee.
+     * @return the class name and bounds together with compartment
+     * visibility.
+     */
+    @Override
+    public String classNameAndBounds() {
+        return super.classNameAndBounds()
+            + "attributesVisible=" + isAttributesVisible() + ";";
+    }
+
+    @Override
+    protected void updateListeners(Object oldOwner, Object newOwner) {
+        Set<Object[]> listeners = new HashSet<Object[]>();
+
+        // Collect the set of model elements that we want to listen to
+        if (newOwner != null) {
+            // TODO: Because we get called on each and every change event, when
+            // the model is in a state of flux, we'll often get an
+            // InvalidElementException before we finish this collection. The
+            // only saving grace is that we're called SO many times that on the
+            // last time, things should be stable again and we'll get a good set
+            // of elements for the final update.  We need a better mechanism.
+            
+            // add the listeners to the newOwner
+            listeners.add(new Object[] {newOwner, null});
+            
+            // and its stereotypes
+            // TODO: Aren't stereotypes handled elsewhere?
+            for (Object stereotype 
+                    : Model.getFacade().getStereotypes(newOwner)) {
+                listeners.add(new Object[] {stereotype, null});
+            }
+
+            // and its features
+            for (Object feat : Model.getFacade().getFeatures(newOwner)) {
+                listeners.add(new Object[] {feat, null});
+                // and the stereotypes of its features
+                for (Object stereotype 
+                        : Model.getFacade().getStereotypes(feat)) {
+                    listeners.add(new Object[] {stereotype, null});
+                }
+                // and the parameter of its operations
+                if (Model.getFacade().isAOperation(feat)) {
+                    for (Object param : Model.getFacade().getParameters(feat)) {
+                        listeners.add(new Object[] {param, null});
+                    }
+                }
+            }
+        }
+        
+        // Update the listeners to match the desired set using the minimal
+        // update facility
+        updateElementListeners(listeners);
+    }
+    
+    @Override
+    public void renderingChanged() {
+        super.renderingChanged();
+        if (getOwner() != null) {
+            // TODO: We shouldn't actually have to do all this work
+            updateAttributes();
+        }
+    }
+    
+    /*
+     * TODO: Based on my comments below, with that work done,
+     * this method can be removed - Bob.
+     */
+    @Override
+    protected void updateLayout(UmlChangeEvent event) {
+        super.updateLayout(event);
+
+        if (event instanceof AttributeChangeEvent) {
+            Object source = event.getSource();
+            if (Model.getFacade().isAAttribute(source)) {
+                // TODO: We just need to get someone to rerender a single line
+                // of text which represents the element here, but I'm not sure
+                // how to do that. - tfm
+                // TODO: Bob replies - we shouldn't be interested in this event
+                // here. The FigFeature (or its notation) should be listen for
+                // change and the FigFeature should be update from that.
+                updateAttributes();
+            }
+        } else if (event instanceof AssociationChangeEvent 
+                && getOwner().equals(event.getSource())) {
+            Object o = null;
+            if (event instanceof AddAssociationEvent) {
+                o = event.getNewValue();
+            } else if (event instanceof RemoveAssociationEvent) {
+                o = event.getOldValue();
+            }
+            if (Model.getFacade().isAAttribute(o)) {
+                // TODO: Bob says - we should not be listening here for
+                // addition and removal of attributes. This should be done in
+                // FigAttributesCompartment.
+                updateAttributes();
+            }
+        }
+    }
+
+    /**
+     * Updates the attributes in the fig. Called from modelchanged if there is
+     * a modelEvent effecting the attributes and from renderingChanged in all
+     * cases.
+     * TODO: Looks like this should be private - Bob.
+     */
+    protected void updateAttributes() {
+        if (!isAttributesVisible()) {
+            return;
+        }
+        attributesFigCompartment.populate();
+
+        // TODO: make setBounds, calcBounds and updateBounds consistent
+        setBounds(getBounds());
+    }
+    
+    /**
+     * @param isVisible true if the attribute compartment is visible
+     *
+     * @see org.argouml.uml.diagram.AttributesCompartmentContainer#setAttributesVisible(boolean)
+     */
+    public void setAttributesVisible(boolean isVisible) {
+        setCompartmentVisible(attributesFigCompartment, isVisible);
+    }
+
+    /**
+     * @return the Fig for the Attribute compartment
+     */
+    public FigAttributesCompartment getAttributesCompartment() {
+        // Set bounds will be called from our superclass constructor before
+        // our constructor has run, so make sure this gets set up if needed.
+        if (attributesFigCompartment == null) {
+            attributesFigCompartment = new FigAttributesCompartment(
+                    getOwner(),
+                    DEFAULT_COMPARTMENT_BOUNDS, 
+                    getSettings());
+        }
+        return attributesFigCompartment;
+    }
+    
+}
---
+++ releases/VERSION_0_29_1/src/argouml-app/src/org/argouml/uml/diagram/ui/FigCompartment.java
@@ -0,0 +1,191 @@
+// $Id$
+// Copyright (c) 1996-2009 The Regents of the University of California. All
+// Rights Reserved. Permission to use, copy, modify, and distribute this
+// software and its documentation without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph appear in all copies.  This software program and
+// documentation are copyrighted by The Regents of the University of
+// California. The software program and documentation are supplied "AS
+// IS", without any accompanying services from The Regents. The Regents
+// does not warrant that the operation of the program will be
+// uninterrupted or error-free. The end-user understands that the program
+// was developed for research purposes and is advised not to rely
+// exclusively on the program for any reason.  IN NO EVENT SHALL THE
+// UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+// SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+// UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+package org.argouml.uml.diagram.ui;
+
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.util.Collection;
+import java.util.List;
+
+import org.argouml.uml.diagram.DiagramSettings;
+import org.tigris.gef.presentation.Fig;
+import org.tigris.gef.presentation.FigRect;
+
+/**
+ * Presentation logic for a UML List Compartment. <p>
+ * 
+ * The UML defines a Name Compartment, and a List Compartment. 
+ * This class implements the latter.<p>
+ * 
+ * A List Compartment is a boxed compartment,
+ * containing vertically stacked figs,
+ * which is common to e.g. an operations
+ * compartment and an attributes compartment.<p>
+ * 
+ * The bigPort is filled, but has no border. All other figs contained 
+ * in this group may not be filled, but can have a border. <p>
+ * 
+ * The size calculation done here supports vertically 
+ * stacked sub-figs of this group and supports all 
+ * compartment specializations.
+ * 
+ * @author Bob Tarling
+ */
+public abstract class FigCompartment extends ArgoFigGroup {
+
+    private Fig bigPort;
+
+    /**
+     * The constructor.
+     *
+     * @param x x
+     * @param y y
+     * @param w width
+     * @param h height
+     * @deprecated for 0.27.3 by tfmorris.  Use 
+     * {@link #FigCompartment(Object, Rectangle, DiagramSettings)}.
+     */
+    @SuppressWarnings("deprecation")
+    @Deprecated
+    public FigCompartment(int x, int y, int w, int h) {
+        constructFigs(x, y, w, h);
+    }
+
+    private void constructFigs(int x, int y, int w, int h) {
+        bigPort = new FigRect(x, y, w, h, LINE_COLOR, FILL_COLOR);
+        bigPort.setFilled(true);
+        bigPort.setLineWidth(0);
+
+        addFig(bigPort);
+    }
+    
+    /**
+     * Construct a new FigCompartment.
+     * 
+     * @param owner owning UML element
+     * @param bounds rectangle describing bounds of compartment
+     * @param settings render settings
+     */
+    public FigCompartment(Object owner, Rectangle bounds,
+            DiagramSettings settings) {
+        super(owner, settings);
+        constructFigs(bounds.x, bounds.y, bounds.width, bounds.height);
+    }
+
+    /**
+     * @return the bigPort
+     */
+    public Fig getBigPort() {
+        return bigPort;
+    }
+
+    /**
+     * The minimum width is the minimum width of the child with the widest
+     * minimum width.
+     * The minimum height is the total minimum height of all child figs.
+     * @return the minimum width
+     */
+    @Override
+    public Dimension getMinimumSize() {
+        int minWidth = 0;
+        int minHeight = 0;
+        for (Fig fig : (Collection<Fig>) getFigs()) {
+            if (fig.isVisible() && fig != getBigPort()) {
+                int fw = fig.getMinimumSize().width;
+                if (fw > minWidth) {
+                    minWidth = fw;
+                }
+                minHeight += fig.getMinimumSize().height;
+            }
+        }
+
+        minHeight += 2; // 2 Pixel padding after compartment
+        return new Dimension(minWidth, minHeight);
+    }
+
+    @Override
+    protected void setBoundsImpl(int x, int y, int w, int h) {
+        Rectangle oldBounds = getBounds();
+
+        Dimension minimumSize = getMinimumSize();
+        int newW = Math.max(w, minimumSize.width);
+        int newH = Math.max(h, minimumSize.height);
+
+        int currentHeight = 0;
+
+        for  (Fig fig : (List<Fig>) getFigs()) {
+            if (fig.isVisible() && fig != getBigPort()) {
+                int fh = fig.getMinimumSize().height;
+
+                fig.setBounds(x, y + currentHeight, newW, fh);
+                currentHeight += fh;
+            }
+        }
+        getBigPort().setBounds(x, y, newW, newH);
+        calcBounds();
+        firePropChange("bounds", oldBounds, getBounds());
+    }
+    
+    /**
+     * Create a new model element for the compartment.
+     */
+    protected abstract void createModelElement();
+
+    @Override
+    public void setLineWidth(int w) {
+        super.setLineWidth(w);
+        bigPort.setLineWidth(0);
+    }
+
+    @Override
+    public void setFilled(boolean f) {
+        // Only the bigPort may be filled
+        super.setFilled(false);
+        bigPort.setFilled(f);
+    }
+
+    @Deprecated //see parent
+    @Override
+    public boolean getFilled() {
+        return isFilled();
+    }
+
+    @Override
+    public boolean isFilled() {
+        return bigPort.isFilled();
+    }
+
+    /**
+     * This operation shall return a name unique for this type of 
+     * compartment. Potential use: show at the top in the compartment 
+     * as described in the UML, or as an identification string for 
+     * the compartment type. <p>
+     * See UML 1.4.2 OMG, chapter 5.24.1.2: Compartment name.
+     * 
+     * @return the name of the compartment 
+     */
+    public abstract String getName();
+    
+}
